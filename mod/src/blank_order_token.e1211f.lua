0c91b2c12b06caee0a1e48affc6a739a48111d5a
aldaranmoses <aldaran.moses@gmail.com>
2020-04-19 12:37:21 +0200

    Clean up, renamed some functions, replaced player color check with is_player_permitted() function. Added bag of "Blank Order Tokens"

--- mod/src/blank_order_token.e1211f.lua
+++ mod/src/blank_order_token.e1211f.lua
@@ -1,21 +1,20 @@
--- Blank Order Token
-
+--[[ Blank Order Token --]]
 local unit_info_table = Global.getTable("unitInfo")
 local unit_card_info_table = Global.getTable("cardInfo")
 local list_builder_table = Global.getTable("listBuilder")
 local command_token_data_table = Global.getTable("commandTokenData")
 
 function onLoad()
-    create_assign_button()
+    create_buttons()
 end
 
-function create_assign_button()
+function create_buttons()
     self.createButton(
         {
             click_function = "assign_unit_to_this_token",
             function_owner = self,
             label = "Assign",
-            position = {0, 0.05, 0.2},
+            position = {0, 0.05, -0.2},
             width = 500,
             height = 100
         }
@@ -23,63 +22,79 @@ function create_assign_button()
 
     self.createButton(
         {
-            click_function = "assign_new_commander",
+            click_function = "promote_new_commander",
             function_owner = self,
             label = "Promote",
-            position = {0, 0.05, -0.2},
+            position = {0, 0.05, 0.2},
             width = 500,
             height = 100
         }
     )
 end
 
+--[[ Determine the unit and set up the required Order Token --]]
 function assign_unit_to_this_token(token, color, alt_click)
-    if Player.Red or Player.Blue then
-        local objects = Player[color].getSelectedObjects()
+    if not is_player_permitted(color) then
+        broadcastToColor("Only the Blue or Red Player can do this.", color, {r = 1, g = 0, b = 0})
+        return
+    end
 
-        if #objects ~= 2 then
-            broadcastToColor(
-                "You must select (Ctrl+Click) this token AND only the unit leader mini.",
-                color,
-                {r = 1, g = 0, b = 0}
-            )
-            return
-        end
+    local objects = Player[color].getSelectedObjects()
 
-        for _, object in ipairs(objects) do
-            if object.getGUID() ~= self.getGUID() then
-                if object.getVar("unitName") and not object.getVar("commandType") then
-                    local unit_name = object.getVar("unitName")
-                    local unit_command_type = unit_info_table[unit_name].commandType
-                    local unit_token_name = unit_card_info_table.unitCards[unit_name].tokenName
+    if #objects ~= 2 then
+        broadcastToColor(
+            "You must select this token AND the unit leader mini.\n (Use Ctrl+Left Click to select objects)",
+            color,
+            {r = 1, g = 0, b = 0}
+        )
+        return
+    end
 
-                    update_token(unit_name, unit_command_type, unit_token_name, color)
-                end
+    for _, object in ipairs(objects) do
+        if object.getGUID() ~= self.getGUID() then
+            if object.getVar("unitName") and not object.getVar("commandType") then
+                local unit_name = object.getVar("unitName")
+                local unit_command_type = unit_info_table[unit_name].commandType
+                local unit_token_name = unit_card_info_table.unitCards[unit_name].tokenName
+
+                update_token(unit_name, unit_command_type, unit_token_name, color)
             end
         end
     end
 end
 
-function assign_new_commander(token, color, alt_click)
-    if Player.Red or Player.Blue then
-        local objects = Player[color].getSelectedObjects()
+--[[ Determine the unit, promote to Commander Rank and replace the Order Token --]]
+function promote_new_commander(token, color, alt_click)
+    if not is_player_permitted(color) then
+        broadcastToColor("Only the Blue or Red Player can do this.", color, {r = 1, g = 0, b = 0})
+        return
+    end
 
-        if #objects ~= 2 then
-            broadcastToColor(
-                "You must select (Ctrl+Click) this token AND only the unit leader mini.",
-                color,
-                {r = 1, g = 0, b = 0}
-            )
-            return
-        end
+    local objects = Player[color].getSelectedObjects()
 
-        for _, object in ipairs(objects) do
-            if object.getGUID() ~= self.getGUID() then
-                if object.getVar("unitName") then
+    if #objects ~= 3 then
+        broadcastToColor(
+            "You must select this token AND the unit leader mini AND the units existing Order Token.\n(Use Ctrl+Left Click to select objects)",
+            color,
+            {r = 1, g = 0, b = 0}
+        )
+        return
+    end
+
+    for _, object in ipairs(objects) do
+        if object.getGUID() ~= self.getGUID() then
+            if object.getVar("unitName") then
+                -- If this is the existing Order Token then destroy it.
+                if object.getVar("commandType") then
+                    destroyObject(object)
+                else
+                    -- Set up the new Order Token.
                     local unit_name = object.getVar("unitName")
                     local unit_command_type = unit_info_table[unit_name].commandType
                     local new_unit_command_type = unit_command_type:match("^%l+") .. "Commander"
-                    local unit_token_name = new_unit_command_type:match('^%l+'):gsub("^%l", string.upper) .. " Commander Command Token (Promoted)" -- unit_card_info_table.unitCards[unit_name].tokenName
+                    local unit_token_name =
+                        new_unit_command_type:match("^%l+"):gsub("^%l", string.upper) ..
+                        " Commander Command Token (Promoted)" -- unit_card_info_table.unitCards[unit_name].tokenName
 
                     update_token(unit_name, new_unit_command_type, unit_token_name, color)
                     spawn_commander_token(object)
@@ -89,6 +104,7 @@ function assign_new_commander(token, color, alt_click)
     end
 end
 
+--[[ Turns this token into the required Order Token --]]
 function update_token(unit_name, unit_command_type, unit_token_name, color)
     -- Set Custom Object properties.
     self.setCustomObject(
@@ -101,10 +117,11 @@ function update_token(unit_name, unit_command_type, unit_token_name, color)
         }
     )
 
+    -- Set object properties
     self.setName(color .. " " .. unit_token_name)
     self.setDescription("")
 
-    -- Set Script.
+    -- Set the objects script.
     local luaScript =
         "unitName = '" ..
         unit_name ..
@@ -115,6 +132,7 @@ function update_token(unit_name, unit_command_type, unit_token_name, color)
     self.reload()
 end
 
+--[[ Grab a Commander Token from the bag and place it next to the specified object --]]
 function spawn_commander_token(object)
     base_position = object.getPosition()
     base_rotation = object.getRotation()
@@ -127,7 +145,16 @@ function spawn_commander_token(object)
     command_token_bag.takeObject(
         {
             position = {base_position.x + b, base_position.y + 1, base_position.z + a},
-            -- rotation = {0, 90, 0}
+            rotation = {0, base_rotation.y, 0}
         }
     )
+end
+
+--[[ Return true if the Player color is permitted. --]]
+function is_player_permitted(color)
+    if color == "Blue" or color == "Red" then
+        return true
+    else
+        return false
+    end
 end
