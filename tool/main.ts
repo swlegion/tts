cc15ce513ccef85ec7039124861d3c0258c2de03
Matan Lurey <matan@lurey.org>
2021-02-13 11:59:45 -0800

    Fix bug.

--- tool/main.ts
+++ tool/main.ts
@@ -10,18 +10,18 @@ require('make-promises-safe');
   const positional = args._;
   switch (positional[0]) {
     case 'compile':
-      console.info(`Compiling "${positional[2]}" -> "${positional[1]}"...`);
-      await compileSaveFile(positional[2], positional[1]);
+      console.info(`Compiling "${positional[1]}" -> "${positional[2]}"...`);
+      await compileSaveFile(positional[1], positional[2]);
       break;
     case 'extract':
       console.info(`Extracting "${positional[1]}" -> "${positional[2]}"`);
       await extractSaveFile(positional[1], positional[2]);
       break;
     case 'develop':
-      console.info(`Compiling "${positional[2]}" -> "${positional[1]}"...`);
-      await compileSaveFile(positional[2], positional[1]);
+      console.info(`Compiling "${positional[1]}" -> "${positional[2]}"...`);
+      await compileSaveFile(positional[1], positional[2]);
       const symLink = await createSymlink();
-      console.info(`Created "${symLink}" -> "${positional[1]}"`);
+      console.info(`Created "${symLink}" -> "${positional[2]}"`);
       break;
     default:
       console.error('Unknown command', args);
