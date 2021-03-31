e155427eaee22b64095dfb2f7857236e872337db
Matan Lurey <matan@lurey.org>
2021-03-31 16:08:11 -0700

    Still not working.

--- mod/src/StarWarsLegion.lua
+++ mod/src/StarWarsLegion.lua
@@ -9,7 +9,7 @@
 function onLoad()
     CCID = sha256(tostring(Time.time))
     UUID = sha256(Player.getPlayers()[1].steam_id)
-    ga_event("Global", "Load")
+    ga_view("Load")
 
     gameDataGUID = "6bb850"
     battlefieldZoneGUID = "cbc575" -- real
