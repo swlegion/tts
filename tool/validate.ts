fe31aa1703f73bf41b3075ca9cb388c217a3a536
Matan Lurey <matan@lurey.org>
2021-09-26 11:44:55 -0700

    Try to validate save files (not passing).

--- tool/validate.ts
+++ tool/validate.ts
@@ -1,3 +1,5 @@
+import fs from 'fs-extra';
+import path from 'path';
 import { exec, exit } from 'shelljs';
 
 /**
@@ -18,15 +20,40 @@ function runTool(name: string, ...args: string[]): void {
   }
 }
 
-// Validate that the contrib files are valid.
 const isWindows = process.platform === 'win32';
 
-runTool(
-  `ajv${isWindows ? '.cmd' : ''}`,
-  '-d "./contrib/cards.json"',
-  '-s "./mod/schema/CardSet.json"',
-  '-r "./mod/schema/deps/**.json"',
-  '--strict',
-  '--errors=line',
-  '--changes=line',
-);
+// Validate that the contrib files are valid.
+(() => {
+  runTool(
+    `ajv${isWindows ? '.cmd' : ''}`,
+    '-d "./contrib/cards.json"',
+    '-s "./mod/schema/CardSet.json"',
+    // https://github.com/ajv-validator/ajv-cli/issues/172
+    '-r "./mod/schema/deps/**.json"',
+    '--strict',
+    '--errors=line',
+    '--changes=line',
+  );
+})();
+
+// Validate that the save file(s) are valid.
+(() => {
+  // https://github.com/ajv-validator/ajv-cli/issues/172
+  const saveFormatDir = './node_modules/@matanlurey/tts-save-format/src/schema';
+  const recursiveDeps = fs
+    .readdirSync(saveFormatDir)
+    .filter((f) => f.endsWith('.json') && path.basename(f) !== 'SaveState.json')
+    .map((f) => `-r "${saveFormatDir}/${f}"`);
+
+  runTool(
+    `ajv${isWindows ? '.cmd' : ''}`,
+    '-d "./dist/StarWarsLegion.json"',
+    '-s "./node_modules/@matanlurey/tts-save-format/src/schema/SaveState.json"',
+    ...recursiveDeps,
+    '--strict-required=false',
+    '--strict-schema=false',
+    '--remove-additional=failing',
+    '--errors=line',
+    '--changes=line',
+  );
+})();
