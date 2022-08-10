774aeb5a9e93b483dfea5afa943dce9dea161f24
ben-lear <benlear.pv@gmail.com>
2022-08-10 10:45:47 -0400

    Add validation to Unit.json and Units.json files

--- tool/validate.ts
+++ tool/validate.ts
@@ -21,6 +21,7 @@ function runTool(name: string, ...args: string[]): void {
 // Validate that the contrib files are valid.
 const isWindows = process.platform === 'win32';
 
+// Validate homebrew.json and official.json files
 runTool(
   `ajv${isWindows ? '.cmd' : ''}`,
   '-d "./contrib/cards/*.json"',
@@ -29,4 +30,28 @@ runTool(
   '--strict',
   '--errors=line',
   '--changes=line',
+);
+
+// Validate any [...]Units.json files
+// These files should be formatted as a set of Ranks containing units
+runTool(
+    `ajv${isWindows ? '.cmd' : ''}`,
+    '-d "./contrib/cards/**/*Units.json"',
+    '-s "./mod/schema/UnitSet.json"',
+    '-r "./mod/schema/deps/**.json"',
+    '--strict',
+    '--errors=line',
+    '--changes=line',
+);
+
+// Validate any [...]Unit.json files
+// These files should be formatted as a single unit
+runTool(
+    `ajv${isWindows ? '.cmd' : ''}`,
+    '-d "./contrib/cards/**/*Unit.json"',
+    '-s "./mod/schema/Unit.json"',
+    '-r "./mod/schema/deps/**.json"',
+    '--strict',
+    '--errors=line',
+    '--changes=line',
 );
